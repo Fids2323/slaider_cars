436a58a9ea03090eca322a04f1a257b163d5b29b
Dmitriy Khomsky <workfids23@gmail.com>
2022-04-25 22:21:48 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const upBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button') //bring buttons
+
+const sidebar = document.querySelector('.sidebar')
+const mainSlide = document.querySelector('.main-slide')
+const slidesCount = mainSlide.querySelectorAll('div').length //count divs(nomber)
+
+const container = document.querySelector('.container')
+let activeSlideIndex = 0
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh` //4 slaids, need 3
+
+//event buttons, callback
+upBtn.addEventListener('click', () => {
+	changeSlide('up')
+})
+
+downBtn.addEventListener('click', () => {
+	changeSlide('down')
+})
+
+function changeSlide(direction) {
+	if (direction === 'up') {
+		activeSlideIndex++
+		if (activeSlideIndex === slidesCount) {
+			activeSlideIndex = 0
+		}
+	} else if (direction === 'down') {
+		activeSlideIndex--
+		if (activeSlideIndex < 0) {
+			activeSlideIndex = slidesCount - 1
+		}
+	}
+	const height = container.clientHeight
+	mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
+
+	sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
+}
